507d4f10d72a9170fd574793ca26b7a745599b49
seunghwan <slflfl12@naver.com>
2021-01-04 18:01:54 +0900

    test

--- app/src/main/java/io/github/slflfl12/customviewanimation/MainActivity.kt
+++ app/src/main/java/io/github/slflfl12/customviewanimation/MainActivity.kt
@@ -2,15 +2,24 @@ package io.github.slflfl12.customviewanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
+import io.github.slflfl12.customviewanimation.databinding.ActivityMainBinding
 import io.github.slflfl12.rotationlayout.RotationAnimation
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val rotationTextView = RotationAnimation.Builder().setSpeed(3000).setTurn(1).build()
-        rotationTextView.startAnimation(rotation_text_view)
+/*        val rotationTextView = RotationAnimation.Builder().setSpeed(3000).setTurn(1).build()
+        rotationTextView.startAnimation(rotation_text_view)*/
+
+        binding.ivSimple.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.animate_translate)
+            binding.ivSimple.startAnimation(animation)
+        }
+
     }
 }
