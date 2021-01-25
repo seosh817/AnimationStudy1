5975801e300618b3b75e1f4f3c98f240d8e242ee
seunghwan <slflfl12@naver.com>
2021-01-25 20:52:34 +0900

    AnimatedImageView

---
+++ app/src/main/java/io/github/slflfl12/customviewanimation/drawable/AnimatedImageView.kt
@@ -0,0 +1,92 @@
+package io.github.slflfl12.customviewanimation.drawable
+
+import android.content.Context
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+
+open class AnimatedImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    private var anim: AnimationDrawable? = null
+    private var isAttached: Boolean = false
+    private var allowAnimation = true
+
+
+    private var drawableId: Int = 0
+
+    fun setAllowAnimation(allowAnimation: Boolean) {
+        if (this.allowAnimation != allowAnimation) {
+            this.allowAnimation = allowAnimation
+            updateAnim()
+            if(this.allowAnimation.not()) {
+                anim?.setVisible(visibility == View.VISIBLE, true)
+            }
+        }
+    }
+
+    private fun updateAnim() {
+        val drawable = drawable
+        if (isAttached) {
+            anim?.stop()
+        }
+        if (drawable is AnimationDrawable) {
+            setAnimationDrawable(drawable)
+            if(isShown && allowAnimation) {
+                drawable.start()
+            }
+        } else {
+            setAnimationDrawable(null)
+        }
+    }
+
+    protected open fun setAnimationDrawable(drawable: AnimationDrawable?) {
+        this.anim = drawable
+    }
+
+    override fun setImageDrawable(drawable: Drawable?) {
+        if(drawable!= null) {
+            if(drawableId == drawable.hashCode()) return
+            drawableId = drawable.hashCode()
+        } else {
+            drawableId = 0
+        }
+        super.setImageDrawable(drawable)
+        updateAnim()
+    }
+
+    override fun setImageResource(resId: Int) {
+        if(drawableId == resId) return
+        drawableId = resId
+        super.setImageResource(resId)
+        updateAnim()
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        isAttached = true
+        updateAnim()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        if (anim != null) {
+            anim!!.stop()
+        }
+        isAttached = false
+    }
+
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if(isShown && allowAnimation) {
+            anim?.start()
+        } else {
+            anim?.stop()
+        }
+    }
+}
