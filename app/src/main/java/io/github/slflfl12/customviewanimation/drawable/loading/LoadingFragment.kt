9f83ec2600bcbd5c948ebe73a845393c5c9cbb95
seunghwan <slflfl12@naver.com>
2021-02-10 21:04:37 +0900

    contentLoading

--- app/src/main/java/io/github/slflfl12/customviewanimation/drawable/loading/LoadingFragment.kt
+++ app/src/main/java/io/github/slflfl12/customviewanimation/drawable/loading/LoadingFragment.kt
@@ -1,12 +1,25 @@
 package io.github.slflfl12.customviewanimation.drawable.loading
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import io.github.slflfl12.customviewanimation.R
+import io.github.slflfl12.customviewanimation.databinding.FragmentLoadingBinding
 
 class LoadingFragment: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding:FragmentLoadingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_loading, container, false)
+        return binding.root
     }
 
 
