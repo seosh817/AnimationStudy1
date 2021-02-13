da09e6914bae90c15bd4eb350fc3f7d821e999c7
seunghwan <slflfl12@naver.com>
2021-02-13 21:45:52 +0900

    horizontal

--- app/src/main/java/io/github/slflfl12/customviewanimation/drawable/loading/LoadingFragment.kt
+++ app/src/main/java/io/github/slflfl12/customviewanimation/drawable/loading/LoadingFragment.kt
@@ -1,17 +1,29 @@
 package io.github.slflfl12.customviewanimation.drawable.loading
 
+import android.animation.ValueAnimator
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import io.github.slflfl12.customviewanimation.R
 import io.github.slflfl12.customviewanimation.databinding.FragmentLoadingBinding
+import io.github.slflfl12.customviewanimation.interpolator.Interpolators
+import io.github.slflfl12.customviewanimation.util.lerp
 
 class LoadingFragment: Fragment() {
 
     private lateinit var binding:FragmentLoadingBinding
+
+    private val animator = ValueAnimator.ofFloat(0f, 1f).apply {
+        repeatMode = ValueAnimator.REVERSE
+        repeatCount = ValueAnimator.INFINITE
+        duration = 1000L
+        interpolator = Interpolators.ACCELERATE
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +32,31 @@ class LoadingFragment: Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_loading, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        animator.addUpdateListener {
+            binding.pbLoadingHorizontal.setProgress(it.animatedFraction)
+            binding.pbLoadingHorizontalCustom.setProgress(it.animatedFraction)
+
+        }
+    }
+
+    private fun ProgressBar.setProgress(fraction: Float) {
+        progress = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            lerp(min, max, fraction).toInt()
+        } else {
+            lerp(0, max, fraction).toInt()
+        }
+    }
+
+    override fun onDestroyView() {
+        animator.cancel()
+        animator.removeAllUpdateListeners()
+        animator.removeAllListeners()
+        super.onDestroyView()
     }
 
 
---
+++ app/src/main/java/io/github/slflfl12/customviewanimation/drawable/loading/StageView.kt
@@ -0,0 +1,127 @@
+package io.github.slflfl12.customviewanimation.drawable.loading
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.animation.doOnEnd
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import io.github.slflfl12.customviewanimation.R
+import io.github.slflfl12.customviewanimation.databinding.ViewStageBinding
+import io.github.slflfl12.customviewanimation.interpolator.Interpolators
+
+
+class StageView(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attrs, defStyle){
+
+    private var progressAnimator: ValueAnimator? = null
+
+    private val binding: ViewStageBinding = ViewStageBinding.inflate(LayoutInflater.from(context), this, false)
+
+
+
+    private fun startStageAnimation(currentOffSet: Float) {
+        resetStageEventAnimation()
+        startAnimation(currentOffSet)
+    }
+
+    private fun resetStageEventAnimation() {
+        binding.pbStage.setMutateImageResource(R.drawable.stage_progress)
+        binding.progressThumb.run {
+            scaleX = .3f
+            scaleY = .3f
+        }
+
+    }
+
+    private fun ImageView.setMutateImageResource(@DrawableRes resId: Int) {
+        ContextCompat.getDrawable(context, resId)?.let {
+            setImageDrawable(it.mutate()) // mutate -> 동일한 Constant 참조로 인한 drawable변경을 막음
+        }
+    }
+
+    private fun startAnimation(offSet: Float) {
+        progressAnimator = ValueAnimator.ofFloat(0f, offSet).apply {
+            startDelay = 350
+            duration = (1000 * offSet).toLong()
+            interpolator = Interpolators.EASE_IN_CUBIC
+            addUpdateListener {
+                renderProgress(it.animatedFraction)
+            }
+            doOnEnd {
+                renderEnd()
+            }
+            start()
+        }
+    }
+
+    private fun renderProgress(fraction: Float) {
+        binding.pbStage.setImageLevel((1000*fraction).toInt())
+        binding.progressThumb.translationX = binding.pbStage.width * fraction
+        when {
+            fraction >= 1f -> binding.count3.startBounceAnimation()
+            fraction >= .66f -> binding.count2.startBounceAnimation()
+            fraction >= .33f -> binding.count1.startBounceAnimation()
+        }
+    }
+
+    private fun renderEnd() {
+        binding.progressThumb.animate()
+            .setDuration(700)
+            .setInterpolator(Interpolators.EASE_OUT_EXPO)
+            .scaleX(1f)
+            .scaleY(1f)
+            .withEndAction {
+                animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setDuration(330)
+                    .start()
+            }
+    }
+
+    private fun TextView.startBounceAnimation() {
+        if(isSelected) return
+        isSelected = true
+        animate()
+            .scaleX(1.2f)
+            .scaleY(1.2f)
+            .setDuration(70)
+            .setInterpolator(Interpolators.EASE_OUT_EXPO)
+            .withLayer()
+            .withEndAction {
+                animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setDuration(330)
+                    .start()
+            }
+    }
+
+    override fun onDetachedFromWindow() {
+        stopStageEventAnimation()
+        super.onDetachedFromWindow()
+    }
+
+    private fun stopStageEventAnimation() {
+        binding.count1.animate()?.cancel()
+        binding.count2.animate()?.cancel()
+        binding.count3.animate()?.cancel()
+
+
+        progressAnimator?.run {
+            cancel()
+            removeAllUpdateListeners()
+            removeAllListeners()
+        }
+        progressAnimator = null
+    }
+}
