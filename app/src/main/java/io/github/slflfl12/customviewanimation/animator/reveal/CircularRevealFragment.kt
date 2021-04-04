af29a6217259ca865230ff16c69313a6ee884bca
seunghwan <slflfl12@naver.com>
2021-04-04 22:35:10 +0900

    CircularReveal unfold fold

--- app/src/main/java/io/github/slflfl12/customviewanimation/animator/reveal/CircularRevealFragment.kt
+++ app/src/main/java/io/github/slflfl12/customviewanimation/animator/reveal/CircularRevealFragment.kt
@@ -1,15 +1,24 @@
 package io.github.slflfl12.customviewanimation.animator.reveal
 
+import android.animation.Animator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import androidx.core.animation.doOnEnd
+import androidx.core.view.doOnLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.circularreveal.CircularRevealCompat
+import com.google.android.material.circularreveal.CircularRevealLinearLayout
+import com.google.android.material.circularreveal.CircularRevealWidget
 import io.github.slflfl12.customviewanimation.databinding.FragmentAnimatorRevealBinding
+import kotlin.math.hypot
 
 class CircularRevealFragment: Fragment() {
 
     private lateinit var binding: FragmentAnimatorRevealBinding
+    private var radius = 0f
 
 
     override fun onCreateView(
@@ -17,12 +26,70 @@ class CircularRevealFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentAnimatorRevealBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.doOnLayout {
+            radius = it.diagonalLength()
+        }
+        binding.unfold.setOnClickListener {
+            if(it.isSelected) {
+                it.isSelected = false
+                binding.cardRevealView.foldMenu()
+            } else {
+                it.isSelected = true
+                binding.cardRevealView.unFoldMenu()
+            }
+        }
     }
 
 
+
+    private fun View.diagonalLength(): Float {
+        return hypot(width.toFloat(), height.toFloat()) // root(x^2 + y^2)
+    }
+
+    private inline fun CircularRevealWidget.createCircularRevealCompatOf(
+            target: View,
+            startRadius: Float,
+            endRadius: Float,
+            block: Animator.() -> Unit
+    ): Animator {
+        return CircularRevealCompat.createCircularReveal(
+            this,
+            target.centerX().toFloat(),
+            target.centerY().toFloat(),
+            startRadius, endRadius
+        ).apply(block)
+    }
+
+    private fun View.centerX(): Int {
+        return (right + left) / 2
+    }
+
+    private fun View.centerY(): Int {
+        return (bottom + top) / 2
+    }
+
+    private fun CircularRevealLinearLayout.foldMenu() {
+        createCircularRevealCompatOf(
+            binding.unfold, radius, 0f
+        ) {
+            duration = 500
+            doOnEnd {
+                visibility = View.GONE
+            }
+        }.start()
+    }
+
+    private fun CircularRevealLinearLayout.unFoldMenu() {
+        visibility = View.VISIBLE
+        createCircularRevealCompatOf(binding.unfold, 0f, radius) {
+            duration = 500
+        }.start()
+    }
 }
+
