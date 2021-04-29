4a5990cdec442b158159f582a89fc0c1fc316e0e
seunghwan <slflfl12@naver.com>
2021-04-29 22:49:06 +0900

    Create Fragment SLA

--- app/src/main/java/io/github/slflfl12/customviewanimation/sla/StateListAnimatorFragment.kt
+++ app/src/main/java/io/github/slflfl12/customviewanimation/sla/StateListAnimatorFragment.kt
@@ -1,6 +1,24 @@
 package io.github.slflfl12.customviewanimation.sla
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import io.github.slflfl12.customviewanimation.R
+import io.github.slflfl12.customviewanimation.databinding.FragmentSlaBinding
 
 class StateListAnimatorFragment: Fragment() {
+
+    private lateinit var binding: FragmentSlaBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sla, container, false)
+        return binding.root
+    }
 }
