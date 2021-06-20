8df23322c24aaaed2f7e988b68fd53b938690d68
seunghwan <slflfl12@naver.com>
2021-06-20 19:49:50 +0900

    Transition Fragment 생성

---
+++ app/src/main/java/io/github/slflfl12/customviewanimation/transition/TransitionFragment.kt
@@ -0,0 +1,28 @@
+package io.github.slflfl12.customviewanimation.transition
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import io.github.slflfl12.customviewanimation.R
+import io.github.slflfl12.customviewanimation.databinding.FragmentTransitionBinding
+
+class TransitionFragment: Fragment() {
+
+    private lateinit var binding: FragmentTransitionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_transition, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
